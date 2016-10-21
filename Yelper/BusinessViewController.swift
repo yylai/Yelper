8dc3c3c8c72670b44147834d024d914ea2d0c5a0
Yin Lai <yylai.git@gmail.com>
2016-10-20 23:42:22 -0700

    added search bar

--- Yelper/BusinessViewController.swift
+++ Yelper/BusinessViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class BusinessViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BusinessViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     @IBOutlet weak var businessTableView: UITableView!
     
     var businesses: [Business]!
+    
+    var searchBar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +40,7 @@ class BusinessViewController: UIViewController, UITableViewDataSource, UITableVi
         businessTableView.rowHeight = UITableViewAutomaticDimension
         businessTableView.estimatedRowHeight = 120
         
+        setupSearchBar()
         
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
@@ -50,6 +53,54 @@ class BusinessViewController: UIViewController, UITableViewDataSource, UITableVi
          }
          */
     }
+    
+    func setupSearchBar() {
+        self.searchBar = UISearchBar()
+        self.searchBar.sizeToFit()
+        self.searchBar.delegate = self
+        
+        navigationItem.titleView = searchBar
+        
+    }
+    
+    public func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        
+//        if searchText == "" {
+//            filteredMovies = movies!
+//        } else {
+//            filteredMovies = movies!.filter({ (movie) -> Bool in
+//                let currentTitle = movie["title"] as! String
+//                let range = currentTitle.range(of: searchText, options: .caseInsensitive)
+//                return range != nil
+//            })
+//        }
+//        
+//        self.movieTableView.reloadData()
+    }
+    
+    public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        isSearching = true
+    }
+    
+    public func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchBar.text = nil
+        searchBar.setShowsCancelButton(false, animated: true)
+//        isSearching = false
+//        // Remove focus from the search bar.
+  searchBar.endEditing(true)
+    }
+    
+    public func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+//        isSearching = true
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    public func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+//        self.movieTableView.reloadData()
+    }
+    
+    
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if businesses != nil {
